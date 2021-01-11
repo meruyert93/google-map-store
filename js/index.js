28c3826a0b6f7cc1910bc55f26260736e5d929b2
Meruyert Karim <meruyert.karim@nu.edu.kz>
2021-01-11 11:53:06 +0100

    map style default

--- js/index.js
+++ js/index.js
@@ -10,87 +10,86 @@ function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: losAngeles,
     zoom: 8,
-    styles: [
-        {
-            "featureType": "administrative",
-            "elementype": "labels.text.fill",
-            "stylers": [
-                {
-                    "color": "#444444"
-                }
-            ]
-        },
-        {
-            "featureType": "landscape",
-            "elementType": "all",
-            "stylers": [
-                {
-                    "color": "#f2f2f2"
-                }
-            ]
-        },
-        {
-            "featureType": "poi",
-            "elementType": "all",
-            "stylers": [
-                {
-                    "visibility": "off"
-                }
-            ]
-        },
-        {
-            "featureType": "road",
-            "elementType": "all",
-            "stylers": [
-                {
-                    "saturation": -100
-                },
-                {
-                    "lightness": 45
-                }
-            ]
-        },
-        {
-            "featureType": "road.highway",
-            "elementType": "all",
-            "stylers": [
-                {
-                    "visibility": "simplified"
-                }
-            ]
-        },
-        {
-            "featureType": "road.arterial",
-            "elementType": "labels.icon",
-            "stylers": [
-                {
-                    "visibility": "off"
-                }
-            ]
-        },
-        {
-            "featureType": "transit",
-            "elementType": "all",
-            "stylers": [
-                {
-                    "visibility": "off"
-                }
-            ]
-        },
-        {
-            "featureType": "water",
-            "elementType": "all",
-            "stylers": [
-                {
-                    "color": "#46bcec"
-                },
-                {
-                    "visibility": "on"
-                }
-            ]
-        }
-    ]
-    
+    // styles: [
+    //     {
+    //         "featureType": "administrative",
+    //         "elementype": "labels.text.fill",
+    //         "stylers": [
+    //             {
+    //                 "color": "#444444"
+    //             }
+    //         ]
+    //     },
+    //     {
+    //         "featureType": "landscape",
+    //         "elementType": "all",
+    //         "stylers": [
+    //             {
+    //                 "color": "#f2f2f2"
+    //             }
+    //         ]
+    //     },
+    //     {
+    //         "featureType": "poi",
+    //         "elementType": "all",
+    //         "stylers": [
+    //             {
+    //                 "visibility": "off"
+    //             }
+    //         ]
+    //     },
+    //     {
+    //         "featureType": "road",
+    //         "elementType": "all",
+    //         "stylers": [
+    //             {
+    //                 "saturation": -100
+    //             },
+    //             {
+    //                 "lightness": 45
+    //             }
+    //         ]
+    //     },
+    //     {
+    //         "featureType": "road.highway",
+    //         "elementType": "all",
+    //         "stylers": [
+    //             {
+    //                 "visibility": "simplified"
+    //             }
+    //         ]
+    //     },
+    //     {
+    //         "featureType": "road.arterial",
+    //         "elementType": "labels.icon",
+    //         "stylers": [
+    //             {
+    //                 "visibility": "off"
+    //             }
+    //         ]
+    //     },
+    //     {
+    //         "featureType": "transit",
+    //         "elementType": "all",
+    //         "stylers": [
+    //             {
+    //                 "visibility": "off"
+    //             }
+    //         ]
+    //     },
+    //     {
+    //         "featureType": "water",
+    //         "elementType": "all",
+    //         "stylers": [
+    //             {
+    //                 "color": "#46bcec"
+    //             },
+    //             {
+    //                 "visibility": "on"
+    //             }
+    //         ]
+    //     }
+    // ]
   });
   infoWindow = new google.maps.InfoWindow();
   searchStores();
